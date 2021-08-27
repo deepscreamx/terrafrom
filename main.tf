fed746ca19ec3d567c90e15b7bcb3a20cb54662e
deepscreamx <57757838+deepscreamx@users.noreply.github.com>
2021-08-27 10:36:12 +0300

    Update main.tf
    variable "access_key" {
      default = "AKIAWDT4TYAY6KYEZCMC"
      }
    variable "secret_key" {
      default = "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"
      }
    variable "region" {
      default = "us-west-1"
      }

--- main.tf
+++ main.tf
@@ -1,12 +1,1 @@
-provider "aws" {
-  region  = "us-west-1"
-  access_key = "AKIAWDT4TYAY6KYEZCMC"
-  secret_key = "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"
-}
-resource "aws_instance" "my-first-server" {
-    ami            ="ami-07b068f843ec78e72"
-    instance_type  ="t2.micro"  
-    tags = {
-   Name = "ubuntu1"
-    }
-} 
+
