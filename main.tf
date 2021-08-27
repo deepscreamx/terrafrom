768692f54bb6443b9914cc6c0ad36e4e18977299
deepscreamx <57757838+deepscreamx@users.noreply.github.com>
2021-08-27 10:36:59 +0300

    Create main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
-
+variable "access_key" {
+  default = "AKIAWDT4TYAY6KYEZCMC"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-west-1"
+  }
